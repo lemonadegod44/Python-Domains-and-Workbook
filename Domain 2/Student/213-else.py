73e791bc5605f824bb61ca20faffe0fa498fb905
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-08-26 10:56:08 -0500

    Create 213-else.py

---
+++ Domain 2/Student/213-else.py
@@ -0,0 +1,10 @@
+score = 18000
+
+if score > 10000:
+    print("You have reached level 2")
+elif score > 5000:
+    print("You have reached level 1")
+else:
+    print("You need to socre higher to reacha level")
+
+print("Thank you for playing")
