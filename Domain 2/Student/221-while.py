6569cbea43a459e7deff81ddb0153683840b9109
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-08-27 09:34:51 -0500

    Update 221-while.py

--- Domain 2/Student/221-while.py
+++ Domain 2/Student/221-while.py
@@ -1,7 +1,8 @@
 score = 0
 score_increase = 10
 
-while score <= 100:
+while score < 100:
    print("You have", score, "points.") 
    score += score_increase
 
+print("You have reached 100 points. Congratulations!")
