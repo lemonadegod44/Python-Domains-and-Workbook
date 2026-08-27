a755154d10cbebca77d199daa31bae376af7e41a
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-08-27 09:48:15 -0500

    Update 222-for-each.py

--- Domain 2/Student/222-for-each.py
+++ Domain 2/Student/222-for-each.py
@@ -1,3 +1,6 @@
-coins = ('Bronze','Silver','Gold')
+coins = ('Bronze','Silver','Platinum','Gold')
 for coin in coins:
     print ('You possess a', coin, 'coin.')
+    if coin == 'Platinum':
+        print('Congratulations! You move to the next level!')
+        break
