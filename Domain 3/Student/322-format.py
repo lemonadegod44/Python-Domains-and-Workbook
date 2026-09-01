5434d1706974ffad341d40a61dd96ea5dea6d94a
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-09-01 10:57:27 -0500

    .

--- Domain 3/Student/322-format.py
+++ Domain 3/Student/322-format.py
@@ -4,3 +4,5 @@ player1 = "Stacey"
 
 print("Player1:", player1, "Score:", score, "Level:", level)
 print("{2} has {0} points and has reached level {1}".format(score, level, player1))
+print(f"{player1} has {score} points and has reached level {level}")
+print(f"{player1:<15} {score}")
