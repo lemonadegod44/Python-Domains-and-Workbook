7273e436b1f10cb60a67b77e1f7ef3342d7cf1f4
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-09-01 09:58:01 -0500

    /

--- Domain 3/Student/315-append.py
+++ Domain 3/Student/315-append.py
@@ -1,5 +1,8 @@
-message = open('315-message.txt','w')
-message.write('Testing file for player configuration\n')
-message.write('Testing file for player score')
-message.close()
+#message = open('315-message.txt','w')
+#message.write('Testing file for player configuration\n')
+#message.write('Testing file for player score\n')
+#message.close()
 
+message = open('315-message.txt','a')
+message.write('Spencer\n')
+message.close()
--- Domain 3/Student/322-format.py
+++ Domain 3/Student/322-format.py
@@ -3,3 +3,4 @@ level = 3
 player1 = "Stacey"
 
 print("Player1:", player1, "Score:", score, "Level:", level)
+print("{2} has {0} points and has reached level {1}".format(score, level, player1))
