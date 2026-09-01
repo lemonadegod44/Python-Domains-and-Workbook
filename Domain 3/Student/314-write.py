2a02c0edcf5ceeba91884ed3fa8b04a775006c16
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-09-01 09:10:08 -0500

    /

--- Domain 3/Student/314-write.py
+++ Domain 3/Student/314-write.py
@@ -1,5 +1,5 @@
 message = open('314-message.txt','w')
-message.write('Testing file for player configuration')
+message.write('Testing file for player configuration\n')
 message.write('Testing file for player score')
 message.close()
 
