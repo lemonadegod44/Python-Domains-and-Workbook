90bb762468401995e2c585a0b368e26909dcb381
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-09-10 10:03:07 -0500

    srth

--- Domain 4/Student/424-def.py
+++ Domain 4/Student/424-def.py
@@ -1,5 +1,9 @@
 # build a function to multiply a score and a multiplier
+def calculate_score(score,multiplier):
+    return score * multiplier
 
+print(calculate_score(3500,2))
+print(calculate_score(5500,1.6))
 
 
 
