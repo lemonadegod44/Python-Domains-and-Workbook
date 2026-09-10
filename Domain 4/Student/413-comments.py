42753b86215282c2d01e5784bc00b96ca8b1d62d
lemonade_critic67 <lawton.gilmore@students.cvtech.edu>
2026-09-10 09:24:13 -0500

    h

--- Domain 4/Student/413-comments.py
+++ Domain 4/Student/413-comments.py
@@ -1,10 +1,10 @@
 game_state = True
 game_lives = 1
 
-
+#Prints positions achieved during lives
 while game_lives <= 3:
-    for i in range(1,11): 
+    for i in range(1,11): #ranges are zero-based
         print(f"You have reached position {i} in game life {game_lives}")
     if game_state == True:
         game_lives +=1
-print("Thank you for playing.")
+#print("Thank you for playing.")
